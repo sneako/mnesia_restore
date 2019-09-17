c066b46e502fc79c33302a7aeed0111961c3ae23
Nico <nico.piderman@gmail.com>
2019-09-17 17:20:08 +0200

    backup path should be charlist

--- lib/mnesia_restore.ex
+++ lib/mnesia_restore.ex
@@ -1,47 +1,44 @@
 defmodule MnesiaRestore do
-  def create_backup(path \\ "/tmp") do
-    path
-    |> file_name()
-    |> :mnesia.backup()
-  end
 
-  def from_backup(from, to, backup_source, backup_dest) do
+  def rename_backup(from, to, backup_source, backup_dest) do
     switch = fn
       node when node == from -> to
-      node when node == to -> throw({:error, :already_exists})
       node -> node
     end
 
     convert = fn
       {:schema, :db_nodes, nodes}, acc ->
-        {[:schema, :db_nodes, Enum.map(nodes, switch)], acc}
+        {[{:schema, :db_nodes, Enum.map(nodes, switch)}], acc}
 
       {:schema, :version, version}, acc ->
-        {[:schema, :version, version], acc}
+        {[{:schema, :version, version}], acc}
 
       {:schema, :cookie, cookie}, acc ->
-        {[:schema, :cookie, cookie], acc}
+        {[{:schema, :cookie, cookie}], acc}
 
       {:schema, table, create_list}, acc ->
         keys = [:ram_copies, :disc_copies, :disc_only_copies]
 
-        opt_switch = fn {k, v} ->
-          case Enum.member?(keys, k) do
-            true -> {k, Enum.map(v, switch)}
-            false -> {k, v}
-          end
+        opt_switch = fn
+          {k, {a, v}} ->
+            {k, {a, switch.(v)}}
+
+          {k, v} ->
+            case Enum.member?(keys, k) do
+              true -> {k, Enum.map(v, switch)}
+              false -> {k, v}
+            end
         end
 
-        {[:schema, table, Enum.map(create_list, opt_switch)], acc}
+        {[{:schema, table, Enum.map(create_list, opt_switch)}], acc}
 
       other, acc ->
         {[other], acc}
     end
 
-    :mnesia.traverse_backup(backup_source, __MODULE__, backup_dest, __MODULE__, convert, :switched)
+    :mnesia.traverse_backup(backup_source, backup_dest, convert, :switched)
   end
 
-  defp file_name(path) do
-    "#{path}/mnesia-backup-#{System.system_time(:second)}.#{Node.self()}"
-  end
+  def restore(path_to_renamed, opts \\ [default_op: :recreate_tables]),
+    do: :mnesia.restore(path_to_renamed, opts)
 end
--- test/mnesia_restore_test.exs
+++ test/mnesia_restore_test.exs
@@ -1,4 +1,124 @@
 defmodule MnesiaRestoreTest do
-  use ExUnit.Case
+  use ExUnit.ClusteredCase
   doctest MnesiaRestore
+  alias ExUnit.ClusteredCase.Cluster
+
+  @backup1 "/tmp/mnesia_restore_test_backup1"
+  @renamed1 "/tmp/mnesia_restore_test_renamed_backup1"
+
+  setup do
+    on_exit(fn ->
+      [
+        @backup1,
+        @renamed1
+      ]
+      |> Enum.each(&File.rm/1)
+    end)
+  end
+
+  scenario "given two nodes", cluster_size: 2 do
+    test "create a backup and restore it on a new cluster with recreate_tables option", %{
+      cluster: cluster
+    } do
+      [primary, secondary] = Cluster.members(cluster)
+
+      [
+        {:mnesia, :stop, []},
+        {:mnesia, :create_schema, [[primary]]},
+        {:mnesia, :start, []},
+        create_table_mfa(:test_table, primary)
+      ]
+      |> run_on(primary)
+
+      # insert some rows on primary node
+      for i <- 1..10 do
+        write_mfa(:test_table, i, i)
+      end
+      |> run_on(primary)
+
+      # data exists
+      for i <- 1..10 do
+        {{:mnesia, :dirty_read, [:test_table, i]}, [{:test_table, i, i}]}
+      end
+      |> run_and_assert_on(primary)
+
+      # take a backup
+      [
+        # {:mnesia, :activate_checkpoint, [[{:name, :checkpoint}, {:max, [:test_table]}]]},
+        {:mnesia, :backup, [to_charlist(@backup1)]}
+      ]
+      |> run_on(primary)
+
+      # ensure the backup is good, restore it on the node it just came from
+      [
+        {{:mnesia, :start, []}, :ok},
+        {{:mnesia, :delete_table, [:test_table]}, {:atomic, :ok}},
+        {{MnesiaRestore, :restore, [to_charlist(@backup1)]}, {:atomic, [:test_table]}}
+      ]
+      |> run_and_assert_on(primary)
+
+      for i <- 1..10 do
+        {{:mnesia, :dirty_read, [:test_table, i]}, [{:test_table, i, i}]}
+      end
+      |> run_and_assert_on(primary)
+
+      # rename the backup
+      assert {:ok, :switched} =
+               MnesiaRestore.rename_backup(
+                 primary,
+                 secondary,
+                 to_charlist(@backup1),
+                 to_charlist(@renamed1)
+               )
+
+      [
+        {:mnesia, :stop, []},
+        {:mnesia, :create_schema, [[secondary]]},
+        {:mnesia, :start, []}
+      ]
+      |> run_on(secondary)
+
+      # restore the renamed backup on the secondary node
+      [
+        {{MnesiaRestore, :restore, [to_charlist(@renamed1)]}, {:atomic, [:test_table]}}
+      ]
+      |> run_and_assert_on(secondary)
+
+      # read the data on the new node
+      [{:mnesia, :dirty_all_keys, [:test_table]}]
+      |> run_on(secondary)
+
+      for i <- 1..10 do
+        {{:mnesia, :dirty_read, [:test_table, i]}, [{:test_table, i, i}]}
+      end
+      |> run_and_assert_on(secondary)
+    end
+  end
+
+  defp create_table_mfa(name, node) do
+    {:mnesia, :create_table, [name, [{:attributes, [:id, :value]}, {:disc_copies, [node]}]]}
+  end
+
+  defp write_mfa(table, key, value) do
+    {MnesiaRestoreTest, :write, [table, key, value]}
+  end
+
+  defp run_on(mfas, node) do
+    Enum.each(mfas, fn {m, f, a} ->
+      :rpc.call(node, m, f, a)
+    end)
+  end
+
+  defp run_and_assert_on(mfas, node) do
+    Enum.each(mfas, fn {{m, f, a}, expected} ->
+      assert ^expected = :rpc.call(node, m, f, a)
+    end)
+  end
+
+  def write(table, k, v) do
+    fn ->
+      :mnesia.write({table, k, v})
+    end
+    |> :mnesia.transaction()
+  end
 end
