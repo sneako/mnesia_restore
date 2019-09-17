36d807f22d28f002ea0e7e4852fb7771aa8a4532
Nico <nico.piderman@gmail.com>
2019-09-17 16:48:52 +0200

    rm test

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MnesiaRestore.MixProject do
     [
       app: :mnesia_restore,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
--- test/mnesia_restore_test.exs
+++ test/mnesia_restore_test.exs
@@ -1,8 +1,4 @@
 defmodule MnesiaRestoreTest do
   use ExUnit.Case
   doctest MnesiaRestore
-
-  test "greets the world" do
-    assert MnesiaRestore.hello() == :world
-  end
 end
